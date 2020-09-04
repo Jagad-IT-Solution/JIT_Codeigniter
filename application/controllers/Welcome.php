00c64e9690236517f964168caac7f023ba9649c1
Angga Saputra <34059838+kangangga@users.noreply.github.com>
2020-09-04 15:44:11 +0700

    Fix method pdf

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,8 +5,9 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->set_response(400);
-		var_dump($this->code);
-		$this->load_config();
+		// $this->set_response(400);
+		// var_dump($this->code);
+		// $this->load_config();
+		var_dump($this->pdf->config);
 	}
 }
--- system/libraries/Pdf.php
+++ system/libraries/Pdf.php
@@ -1,9 +1,10 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once(dirname(__FILE__) . '/Dompdf/dompdf_config.inc.php');
+require_once(dirname(__FILE__) . '/Dompdf/autoload.inc.php');
+use Dompdf\Dompdf;
 
-class Pdf extends DOMPDF
+class CI_Pdf extends Dompdf
 {
 	/**
 	 * Reference to the CodeIgniter singleton
@@ -11,6 +12,8 @@ class Pdf extends DOMPDF
 	 * @var object
 	 */
 	protected $ci;
+
+	public $config = [];
 
 	/**
 	 * Class constructor
@@ -20,14 +23,43 @@ class Pdf extends DOMPDF
 	 * @param	array	$config	Parameters
 	 */
 	public function __construct($config = [])
-	{
+	{ 
+		parent::__construct($config);
+		$this->default_config();
+		$this->config = $this->getConfig();
+        log_message('info', 'Dompdf Class Initialized');
 		$this->ci =& get_instance();
 	}
 
-	public function load_view($view, $data = array())
+	public function load_view($view, $data = [])
 	{
-		$html = $this->ci->load->view($view, $data, TRUE);
+		// $html = $this->ci->load->view($view, $data, TRUE);
+		// return $this->load_html($html);
+		// $this->render();
+		// instantiate and use the dompdf class=
+		$this->loadHtml('hello world');
+		// (Optional) Setup the paper size and orientation
+		$this->setPaper('A4', 'landscape');
+		// Render the HTML as PDF
+		return $this->render();
+		// Output the generated PDF to Browser
+		// $this->stream();
+	}
 
-		$this->load_html($html);
+	public function getConfig($value='')
+	{
+		return $this->getOptions();
+	}
+
+	public function set_config($options = [])
+	{
+		$this->setOptions($options);
+	}
+
+	public function default_config($options = [])
+	{
+		$config = $this->getConfig();
+		$config->setIsHtml5ParserEnabled(TRUE);
+		$this->set_config($config);
 	}
 }
